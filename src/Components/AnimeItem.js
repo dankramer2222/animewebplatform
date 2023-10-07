fd7caf3409fac8f8ba11419f8450914457b0615a
Danila Albutov <danil.miheev2222@gmail.com>
2023-10-06 23:00:35 -0700

    5

--- src/Components/AnimeItem.js
+++ src/Components/AnimeItem.js
@@ -1,70 +1,230 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
-function AnimeItem() {
-    const {id}=useParams()
-    
-//state
-const [anime,setAnime] = React.useState({})
-const [characters,setCharacters] = React.useState([])
-const [showMore, setShowMore] = React.useState(false)
+const AnimeItemStyled = styled.div`
+  padding: 3rem 18rem;
+  background-color: #ededed;
 
-    //destructure anime
-    const {
-        title, synopsis, 
-        trailer,duration,aired, 
-        season, images, rank, 
-        score,scored_by, popularity, 
-        status, rating, source } = anime
+  h1 {
+    display: inline-block;
+    font-size: 3rem;
+    margin-bottom: 1.5rem;
+    cursor: pointer;
+    background: linear-gradient( to right, #a855f7, #27ae60);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    transition: all 0.4s ease-in-out;
 
-    //get anime besed by id or title(i didnt choose yet)
-    const getAnime = async(anime)=>{
-        const response = await fetch(`https://api.jikan.moe/v4/anime/${anime}`)
-        const data = await response.json()
-        setAnime(data.data)
-        console.log(data.data)
+    &:hover {
+      transform: skew(-3deg);
+    }
+  }
+
+  .details {
+    background-color: #fff;
+    border-radius: 20px;
+    padding: 2rem;
+    border: 5px solid #e5e7eb;
+
+    .detail {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 2rem;
+
+      img {
+        border-radius: 7px;
+        max-width: 100%;
+        height: auto;
+      }
+    }
+
+    .anime-details {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      p{
+        display:flex;
+        gap: 0.5rem;
+      }
+      p span:first-child{
+        font-weight:600;
+        color:#454e56;
+      }
+    }
+  }
+
+  .description {
+    margin-top: 2rem;
+    color: #6c7983;
+    line-height: 1.7rem;
+
+    button {
+      background-color: transparent;
+      border: none;
+      outline: none;
+      cursor: pointer;
+      font-size: 1.2rem;
+      color: #27ae60;
+      font-weight: 600;
+    }
+  }
+
+  .title {
+    display: inline-block;
+    margin: 3rem 0;
+    font-size: 2rem;
+    cursor: pointer;
+    background: linear-gradient( to right, #a855f7, #27ae60);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  .trailer-con {
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    iframe{
+        outline:none;
+        border: 5px solid #e5e7eb;
+        padding: 1.5rem;
+        border-radius: 10px;
+        background-color: #FFFFFF;
 
     }
-    useEffect(()=>{
-        getAnime(id)
-    }, [])
-        
+  }
+`;
+
+function AnimeItem() {
+  const { id } = useParams();
+
+  //state
+  const [anime, setAnime] = React.useState({});
+  const [characters, setCharacters] = React.useState([]);
+  const [showMore, setShowMore] = React.useState(false);
+
+  //destructure anime
+  const {
+    title,
+    synopsis,
+    trailer,
+    duration,
+    aired,
+    season,
+    images,
+    rank,
+    score,
+    scored_by,
+    popularity,
+    status,
+    rating,
+    source,
+  } = anime;
+
+  //get anime based on id
+  const getAnime = async (anime) => {
+    const response = await fetch(`https://api.jikan.moe/v4/anime/${anime}`);
+    const data = await response.json();
+    setAnime(data.data);
+  };
+
+  //get characters
+  const getCharacters = async (anime) => {
+    const response = await fetch(
+      `https://api.jikan.moe/v4/anime/${anime}/characters`
+    );
+    const data = await response.json();
+    setCharacters(data.data);
+    console.log(data.data);
+  };
+
+  useEffect(() => {
+    getAnime(id);
+    getCharacters(id);
+  }, []);
 
   return (
-    <div>
-        <h1>{title}</h1>
-        <div className='details'>
-            <div className='detail'>
-                <div className='image'>
-                  <img src={images?.jpg.large_image_url} alt="" />
-                </div>
-                  <div className='anime-details'>
-                  <p><span>Aired:</span><span>{aired?.string}</span></p>
-                        <p><span>Rating:</span><span>{rating}</span></p>
-                        <p><span>Rank:</span><span>{rank}</span></p>
-                        <p><span>Score:</span><span>{score}</span></p>
-                        <p><span>Scored By:</span><span>{scored_by}</span></p>
-                        <p><span>Popularity:</span><span>{popularity}</span></p>
-                        <p><span>Status:</span><span>{status}</span></p>
-                        <p><span>Source:</span><span>{source}</span></p>
-                        <p><span>Season:</span><span>{season}</span></p>
-                        <p><span>Duration:</span><span>{duration}</span></p>
-                  </div>
-            </div>
+    <AnimeItemStyled>
+      <h1>{title}</h1>
+      <div className="details">
+        <div className="detail">
+          <div className="image">
+            <img src={images?.jpg.large_image_url} alt="" />
+          </div>
+          <div className="anime-details">
+            <p>
+              <span>Aired:</span>
+              <span>{aired?.string}</span>
+            </p>
+            <p>
+              <span>Rating:</span>
+              <span>{rating}</span>
+            </p>
+            <p>
+              <span>Rank:</span>
+              <span>{rank}</span>
+            </p>
+            <p>
+              <span>Score:</span>
+              <span>{score}</span>
+            </p>
+            <p>
+              <span>Scored By:</span>
+              <span>{scored_by}</span>
+            </p>
+            <p>
+              <span>Popularity:</span>
+              <span>{popularity}</span>
+            </p>
+            <p>
+              <span>Status:</span>
+              <span>{status}</span>
+            </p>
+            <p>
+              <span>Source:</span>
+              <span>{source}</span>
+            </p>
+            <p>
+              <span>Season:</span>
+              <span>{season}</span>
+            </p>
+            <p>
+              <span>Duration:</span>
+              <span>{duration}</span>
+            </p>
+          </div>
         </div>
-        <p className="description">
-        {showMore ? synopsis : (synopsis && synopsis.length > 300 ? synopsis.slice(0, 300) + '...' : synopsis)}
-            
-            <button onClick={()=>{
-                setShowMore(!showMore)
-            }}>{showMore ? 'Show Less': 'Read More'}</button>
-        </p>
-        <h3 className='title'>Trailer</h3>
-        <div className='trailer-con'></div>
-        {trailer && trailer.embed_url && <iframe src={trailer.embed_url} title={title} frameBorder="0" allowFullScreen></iframe>}
+      </div>
+      <p className="description">
+        {showMore
+          ? synopsis
+          : synopsis && synopsis.length > 300
+          ? synopsis.slice(0, 300) + "..."
+          : synopsis}
 
-        </div>
-  )
+        <button
+          onClick={() => {
+            setShowMore(!showMore);
+          }}
+        >
+          {showMore ? "Show Less" : "Read More"}
+        </button>
+      </p>
+      <h3 className="title">Trailer</h3>
+      <div className="trailer-con">
+        {trailer && trailer.embed_url && (
+          <iframe
+            src={trailer.embed_url}
+            title="Inline Frame Example"
+            width="800"
+            height="450"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        )}
+      </div>
+    </AnimeItemStyled>
+  );
 }
 
-export default AnimeItem
+export default AnimeItem;
